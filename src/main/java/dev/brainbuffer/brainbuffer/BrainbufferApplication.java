3a9334e1fca136450f9e07368a79b5c5636f600c
cogniocode <cognioandrey@gmail.com>
2021-10-10 15:18:59 +0600

    Init project

---
+++ src/main/java/dev/brainbuffer/brainbuffer/BrainbufferApplication.java
@@ -0,0 +1,13 @@
+package dev.brainbuffer.brainbuffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainbufferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainbufferApplication.class, args);
+    }
+
+}
