3b3b845c511b3b78b2f04e59bd8b37bcfb69e203
Allakhyarov Eldar <tripenyazaraz@gmail.com>
2021-10-28 14:35:55 +0600

    репозитории

---
+++ src/main/java/dev/brainbuffer/brainbuffer/appointment/AppointmentRepository.java
@@ -0,0 +1,6 @@
+package dev.brainbuffer.brainbuffer.appointment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
